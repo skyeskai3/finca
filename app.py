382539d3c1c2dfb3f0be20aad2230070b0213122
skye <skye@rev11.com>
2024-07-03 15:49:50 -0400

    finca startup page

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        pin = request.form['pin']
+        if pin == '5656':  # Replace 'your_secret_pin' with the actual PIN
+            return redirect(url_for('registro'))
+        else:
+            flash('Invalid PIN')
+    return render_template('login.html')
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        # Get form data
+        finca = request.form['finca']
+        status = request.form['status']
+        dv = request.form['dv']
+        tomo_rollo = request.form['tomo_rollo']
+        folio_doc = request.form['folio_doc']
+        prov = request.form['prov']
+        propietario = request.form['propietario']
+        finca_madre = request.form['finca_madre']
+        imto_anual_dgi = request.form['imto_anual_dgi']
+        dueño_anterior = request.form['dueño_anterior']
+        ubicacion = request.form['ubicacion']
+        vsl = request.form['vsl']
+        valor_ctstral_terreno = request.form['valor_ctstral_terreno']
+        valor_ctstral_mejora = request.form['valor_ctstral_mejora']
+        forma_de_adq = request.form['forma_de_adq']
+        fecha_de_adquis = request.form['fecha_de_adquis']
+        area = request.form['area']
+        precio_por_hect = request.form['precio_por_hect']
+        plano = request.form['plano']
+        exoneracion_inicio = request.form['exoneracion_inicio']
+        exoneracion_expira = request.form['exoneracion_expira']
+        comentarios = request.form['comentarios']
+
+        # Insert data into MySQL
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            INSERT INTO registro_fincas (
+                finca, status, dv, tomo_rollo, folio_doc, prov, propietario, finca_madre,
+                imto_anual_dgi, dueño_anterior, ubicacion, vsl, valor_ctstral_terreno,
+                valor_ctstral_mejora, forma_de_adq, fecha_de_adquis, area, precio_por_hect,
+                plano, exoneracion_inicio, exoneracion_expira, comentarios
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (finca, status, dv, tomo_rollo, folio_doc, prov, propietario, finca_madre, imto_anual_dgi, dueño_anterior, ubicacion, vsl, valor_ctstral_terreno, valor_ctstral_mejora, forma_de_adq, fecha_de_adquis, area, precio_por_hect, plano, exoneracion_inicio, exoneracion_expira, comentarios))
+        mysql.connection.commit()
+        cur.close()
+
+        return render_template('success.html')
+    return render_template('registro.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'skye'
+    MYSQL_PASSWORD = 'j0Hn1423_.X'
+    MYSQL_DB = 'finca'
